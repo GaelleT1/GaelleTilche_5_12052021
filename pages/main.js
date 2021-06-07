d3b7c3b7915b3887d69b737f5dbdecb2a5ac79f1
gaelle <gaellebiz@gmail.com>
2021-06-07 17:19:50 +0200

    main js updated

--- pages/main.js
+++ pages/main.js
@@ -1,3 +1,5 @@
+//definition des variables//
+
 let _id = [];
 let _name = [];
 let description = [];
@@ -5,13 +7,13 @@ let imageUrl = [];
 let price = [];
 let colors = [[],[],[],[]];
 
-
 const teddies = "http://localhost:3000/api/teddies";
 fetch (teddies)
-.then(function(res) {
+  .then(function(res) {
   if (res.ok) {
     return res.json();
-  }
+}
+  
 })
 .then(function(value){
     console.log(value);
@@ -20,30 +22,30 @@ fetch (teddies)
   
 })
 .catch(function(error){});
+//fonction de recuperation des Ours//
 
 function PushTeddies (teddies){
   const placeElement = document.querySelector('.cards');
    console.log(teddies);
+   //boucle 
     teddies.forEach((Element) => {
-    
-
-      let cardsContainer = document.createElement('div');
-let singleCard = document.createElement('div');
-let image = document.createElement('img');
-let cardDescription = document.createElement('div');
-let tedName = document.createElement('h2');
-let tedPrice = document.createElement('p');
-let viewBtn = document.createElement('a');
-let teddyList = ``;
-const cards = document.getElementById('cards');
-console.log(Element);
-cardsContainer.className = "cards__container";
-singleCard.className = 'single__card';
-tedName.className = 'ted__name';
-tedPrice.className = 'ted__price';
-viewBtn.className = 'view__btn';
-image.className = 'card__img';
-cardDescription.className = 'card__description';
+    let cardsContainer = document.createElement('div');
+    let singleCard = document.createElement('div');
+    let image = document.createElement('img');
+    let cardDescription = document.createElement('div');
+    let tedName = document.createElement('h2');
+    let tedPrice = document.createElement('p');
+    let viewBtn = document.createElement('a');
+    let teddyList = ``;
+  const cards = document.getElementById('cards');
+  console.log(Element);
+  cardsContainer.className = "cards__container";
+  singleCard.className = 'single__card';
+  tedName.className = 'ted__name';
+  tedPrice.className = 'ted__price';
+  viewBtn.className = 'view__btn';
+  image.className = 'card__img';
+  cardDescription.className = 'card__description';
 
 singleCard.setAttribute('href', 'pages/product.html');
 viewBtn.setAttribute('href', 'pages/product.html');
