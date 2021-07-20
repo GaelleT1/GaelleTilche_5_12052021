7d17b1cfaa6223b3f2747dbed29ac16763c81094
gaelle <gaellebiz@gmail.com>
2021-07-20 16:55:23 +0200

    correction javascript

--- pages/product.js
+++ pages/product.js
@@ -40,7 +40,7 @@ function viewTeddies(pdtLocation) {
     let tedPrice = document.getElementById('ted__price');
     tedPrice.innerText = pdtLocation.price/100 + '€';
   
-   // ------------------------Choix des options couleur------------------//
+   // ------------------------Choix  deroulant des options couleur------------------//
 
       const select = document.getElementById('select');
       let colors = pdtLocation.colors;
@@ -52,76 +52,9 @@ function viewTeddies(pdtLocation) {
      select.appendChild(option);
       console.log(option.value);
 }
+}
 
 
-   //------------------------------add to CART------------------------------------//
-//creation de l'évenement'envoyer au panier' au clic du btn//
-const addBtn = document.getElementsById('add__btn');
- addBtn[i].addEventListener('click',(event)=>{
-  event.preventDefault();
-//creation du panier
-let cart = document.getelementbyId('cart');
-let items = [];
-//sauver le panier dans local storage
-function save  () {
-  localStorage.setItem("cart", JSON.stringify(cart.items));
-}
- }, //charger le panier depuis local storage
- function Load () {
-  cart.items = localStorage.getItem("cart");
-  if (cart.items == null) { cart.items = []; }
-  else { cart.items = JSON.parse(cart.items); }
-},
- )
-}
-for(let i=0; i<addBtn.length; i++){
-   {
-		if(typeof(Storage) !== 'undefined'){
-				let items = {
-						id:i+1,
-						let name = tedName.innertText;
-						tedPrice.innerText;
-						
-					};//si panier = local storage :push items dans le panier
-				if(JSON.parse(localStorage.getItem('items')) === null){
-					items.push(items);
-					localStorage.setItem("items",JSON.stringify(items));
-					window.location.reload();
-				}else{ 
-					const localItems = JSON.parse(localStorage.getItem("items"));
-					localItems.map(data=>{
-						if(items.id == data.id){
-							items.no = data.no + 1;
-						}else{
-							items.push(data);
-						}
-					});
-					items.push(item);
-					localStorage.setItem('items',JSON.stringify(items));
-					window.location.reload();
-				}
-			}else{
-				console.log(error);
-			}
-		});
-	}
-	//ajouter les donnee au panier
-	const iconShoppingP = document.querySelector('.iconShopping p');
-	let nb = 0;
-	JSON.parse(localStorage.getItem('items')).map(data=>{
-		nb = nb+data.nb
-;	});
-//creation du tableau
-	const cartItems = cartBox.querySelector('table');
-	let datatable = '';
-	dataTable += document.createElement('tr')
-  document.createElement('tr');
-	if(JSON.parse(localStorage.getItem('items'))[0] === null){
-    dataTable += 
-	}else{
-		JSON.parse(localStorage.getItem('items')).map(data=>{
-     
-		});
-	}
-	
-}
+
+ 
+  
